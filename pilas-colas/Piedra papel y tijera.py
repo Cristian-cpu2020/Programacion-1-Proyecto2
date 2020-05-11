9dc87799851d4b3a44e210383045f9c389a3a9bd
Cristian-cpu2020 <60716911+Cristian-cpu2020@users.noreply.github.com>
2020-05-11 15:56:52 -0600

    actualizacion
    Actualizacion

--- pilas-colas/Piedra papel y tijera.py
+++ pilas-colas/Piedra papel y tijera.py
@@ -14,63 +14,76 @@ def jugar():
     print ("2. Papel")
     print ("3. Tijera")
     print ("------------------------------------------")
-    su_opcion = int(input("Ingrese su opcion: "))
-    print ()
-    if su_opcion == respuesta_pc:
+    su_opcion = int(input("Ingrese su opcion: "))    
+    if su_opcion == respuesta_pc:                
+        final = f"""
+Computadora : Igual
+Humano: Igual
+Resultado: Es un empate! """
+        print ("----------------------------")        
+        print (final)
         print ("----------------------------")
-        print ("Computadora: Igual")
-        print ("Humano: Igual")
-        print ("----------------------------")
-        print ("Resultado: Puntos ganados 0")
         
     
     elif su_opcion == 1 and respuesta_pc == 2:
+        final = f"""
+Computadora: Papel
+Humano: Piedra
+Resultado: Gana Computadora!"""
         print ("----------------------------")
-        print ("Computadora: Papel")
-        print ("Humano: Piedra")
+        print (final)       
         print ("----------------------------")
-        print ("Resultado: Gana computadora!")        
         puntos = 2           
         
     elif su_opcion == 1 and respuesta_pc == 3:
+        final = f"""
+Computadora: Tijera
+Humano: Piedra
+Resultado: Gana Humano!"""
         print ("----------------------------")
-        print ("Computadora: Tijera")
-        print ("Humano: Piedra")
-        print ("----------------------------")
-        print ("Resultado: Gana Humano!")        
+        print (final)       
+        print ("----------------------------")           
         puntos = 1                     
         
     elif su_opcion == 2 and respuesta_pc == 1:
-        print ("----------------------------")        
-        print ("Computadora: Piedra")
-        print ("Humano: Papel")
+        final = f"""
+Computadora: Piedra
+Humano: Papel
+Resultado: Gana Humano!"""
         print ("----------------------------")
-        print ("Resultado: Gana Humano!")        
+        print (final)       
+        print ("----------------------------")               
         puntos = 1                         
         
     elif su_opcion == 2 and respuesta_pc == 3:
-        print ("----------------------------")      
-        print ("Computadora: Tijera")
-        print ("Humano: Papel")
+        final = f"""
+Computadora: Tijera
+Humano: Papel
+Resultado: Gana Computadora!"""
         print ("----------------------------")
-        print ("Resultado: Gana Computadora!")                            
+        print (final)       
+        print ("----------------------------")                                 
         puntos = 2
     elif su_opcion == 3 and respuesta_pc == 1:
-        print ("----------------------------")       
-        print ("Computadora: Piedra")
-        print ("Humano: Tijera")
+        final = f"""
+Computadora: Piedra
+Humano: Tijera
+Resultado: Gana Computadora!"""
         print ("----------------------------")
-        print ("Resultado: Gana Computadora!")                           
+        print (final)       
+        print ("----------------------------")                                  
         puntos = 2
     elif su_opcion == 3 and respuesta_pc == 2:
-        print ("----------------------------")       
-        print ("Computadora: Papel")
-        print ("Humano: Tijera")
+        final = f"""
+Computadora: Papel
+Humano: Tijera
+Resultado: Gana Humano!"""
         print ("----------------------------")
-        print ("Resultado: Gana Humano!")          
+        print (final)       
+        print ("----------------------------")                
         puntos = 1                    
         
-    return puntos  
+    return puntos, final
 
 puntos_acumulados = 0
 pc = 0
@@ -85,25 +98,26 @@ while True:
     print ("-------------------------------------")
     opcion = int(input("Seleccione una opcion: "))    
     if opcion == 1:
-        puntos = jugar()
+        puntos, final = jugar()
         if puntos == 1:
             puntos_acumulados +=1
         elif puntos == 2:
             pc += 1
 
-        print ("Resultado Humano: ", [puntos_acumulados],"<><><>", "Puntos PC: ", [pc])       
-        historial.append(f"Puntos Humano: {puntos_acumulados}  -  Puntos Pc: {pc}")             
+        print ("PUNTOS GANADOS: ", [puntos_acumulados],"<><><>", "Puntos PC: ", [pc])       
+        historial.append(final)             
         print ("-------------------------------------")
         print ()
     elif opcion == 2:
         if len(historial) > 0:
             ultima_accion = historial.pop()                      
-            print ("Ultimo resultado: ",ultima_accion)            
-            print ("-------------------------")
-            print (historial)            
-        if len(historial) == 0:
-            print()
+            print ("***Ultimo Resultado***")
+            print (ultima_accion) 
+            print ("PUNTOS GANADOS: ", [puntos_acumulados],"---", "Puntos PC: ", [pc])           
+            print ("-------------------------")                        
+        if len(historial) == 0:            
             print ("El historial esta vacio!")
+            print ("-------------------------")
             print ("-------------------------")
 
        
